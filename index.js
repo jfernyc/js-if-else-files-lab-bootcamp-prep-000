125767f0f7edcfd20a5b1f33836d7834182d3ffe
Jonathan Fernandez <jfern20@gmail.com>
2018-05-19 05:46:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 var name = "Joe" 
-var height = 74
+var height = "74"
 
-if(name==="Joe"){
-  console.log("The name is correct")
-} else{
-  if(height===74){
-    console.log("The height is correct")
-  }
-}
 
 
 
